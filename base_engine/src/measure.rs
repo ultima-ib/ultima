41a29e650862a0fe01fa61c0737083412d500593
Anatoly Bugakov <anatoly.bugakov@outlook.com>
2022-06-24 22:26:41 +0100

    Refactoring: Measures and Params

--- base_engine/src/measure.rs
+++ base_engine/src/measure.rs
@@ -4,5 +4,7 @@ pub struct Measure<'a>{
     pub calculator: Box<dyn FnMut()->Expr + Send + Sync + 'a>,
     pub name: &'a str,
     pub req_columns: Vec<&'a str>,
+    //this field is to restrict aggregation option to certain type only
+    //for example where it makes sence to aggregate with "first" and not "sum"
     pub aggregation: Option<&'a str>,
 }

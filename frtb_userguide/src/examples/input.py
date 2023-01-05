3dcdc8aadc76c9a825aafa03bb0fe24835342935
Anatoly Bugakov <anatoly.bugakov@outlook.com>
2023-01-05 19:37:25 +0000

    book publish

--- frtb_userguide/src/examples/input.py
+++ frtb_userguide/src/examples/input.py
@@ -20,7 +20,7 @@ ds.prepare()
 
 # Let's see what happened
 def diff(df1: pl.DataFrame, df2: pl.DataFrame) -> pl.DataFrame:
-    """Columns in df1 which are not in df2
+    """Columns in df1 which are not in df2 
 
     Args:
         df1 (pl.DataFrame): First to compare

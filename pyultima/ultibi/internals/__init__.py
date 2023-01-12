a1f65bd9cdac591bdc978cd209c0653dc32dc248
Anatoly Bugakov <anatoly.bugakov@outlook.com>
2023-01-12 21:27:51 +0000

    frtb_engine to depend on testpypi

--- pyultima/ultibi/internals/__init__.py
+++ pyultima/ultibi/internals/__init__.py
@@ -3,7 +3,7 @@ Core Ultima functionality.
 
 The modules within `ultima.internals` are interdependent. To prevent cyclical imports,
 they all import from each other via this __init__ file using
-`import ultima.internals as uli`. The imports below are being shared across this module.
+`import ultibi.internals as uli`. The imports below are being shared across this module.
 """
 
 from .agg_request import AggRequest, aggregation_ops

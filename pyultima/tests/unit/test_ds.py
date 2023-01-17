40bbe68ad8db0a4464fe6bbfe02e49fb011748f6
Anatoly Bugakov <anatoly.bugakov@outlook.com>
2023-01-17 21:29:21 +0000

    mypy fix

--- pyultima/tests/unit/test_ds.py
+++ pyultima/tests/unit/test_ds.py
@@ -16,7 +16,7 @@ class TestCreation(unittest.TestCase):
 
         dataset.prepare()
 
-        assert "jurisdiction" in dataset.calc_params
+        assert "jurisdiction" in [cp["name"] for cp in dataset.calc_params]
         assert "FX Curvature KbMinus" in dataset.measures
         assert isinstance(frame, pl.DataFrame)
         assert "TradeId" in frame

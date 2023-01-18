fb0f60f1e063d887138e1b459405ad50217d2163
Anatoly Bugakov <anatoly.bugakov@outlook.com>
2023-01-18 15:47:41 +0000

    python fmt

--- pyultima/ultibi/__init__.py
+++ pyultima/ultibi/__init__.py
@@ -2,10 +2,22 @@
 FRTB usecase specific library which levrages on ultima's base engine
 """
 
-from .internals import (AggRequest, DataSet, FRTBDataSet,
- aggregation_ops, NoDataError, OtherError)
+from .internals import (
+    AggRequest,
+    DataSet,
+    FRTBDataSet,
+    NoDataError,
+    OtherError,
+    aggregation_ops,
+)
 from .internals.execute import execute_agg
 
-__all__ = ["execute_agg", 
-"AggRequest", "FRTBDataSet", "DataSet", 
-"aggregation_ops", "OtherError", "NoDataError"]
+__all__ = [
+    "execute_agg",
+    "AggRequest",
+    "FRTBDataSet",
+    "DataSet",
+    "aggregation_ops",
+    "OtherError",
+    "NoDataError",
+]

c4ac1f37b2b4688c08a0361be457677b35ed8066
Anatoly Bugakov <anatoly.bugakov@outlook.com>
2023-03-16 15:05:32 +0000

    #122

--- ultibi/ultibi_server/build.rs
+++ ultibi/ultibi_server/build.rs
@@ -4,5 +4,5 @@ fn main() -> std::io::Result<()> {
     let path =
         std::env::var("STATIC_FILES_DIR").unwrap_or_else(|_| "../../frontend/dist".to_string());
 
-    resource_dir(path).build()
+    resource_dir(dbg!(path)).build()
 }

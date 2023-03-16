c012c7009f4ae3de3cdde5c13c6436e5e9a81db2
Anatoly Bugakov <anatoly.bugakov@outlook.com>
2023-03-16 15:52:31 +0000

    #122

--- driver/build.rs
+++ driver/build.rs
@@ -1,16 +1,6 @@
-use std::path::Path;
-
 use static_files::resource_dir;
 
 fn main() -> std::io::Result<()> {
-    let path = std::env::var("STATIC_FILES_DIR").unwrap_or_else(|_| {
-        Path::new("..")
-            .join("frontend")
-            .join("dist")
-            .into_os_string()
-            .into_string()
-            .unwrap()
-    });
-
+    let path = std::env::var("STATIC_FILES_DIR").unwrap_or_else(|_| "../frontend/dist".to_string());
     resource_dir(dbg!(path)).build()
 }

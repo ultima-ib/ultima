b53f8b2066f6526abccfeb39d0c9034a4825547f
Anatoly Bugakov <anatoly.bugakov@outlook.com>
2023-03-16 15:05:04 +0000

    #122

--- ultibi/ultibi_server/build.rs
+++ ultibi/ultibi_server/build.rs
@@ -3,37 +3,6 @@ use static_files::resource_dir;
 fn main() -> std::io::Result<()> {
     let path =
         std::env::var("STATIC_FILES_DIR").unwrap_or_else(|_| "../../frontend/dist".to_string());
-        let paths = std::fs::read_dir("./").unwrap();
-
-        dbg!("this level");
-
-        for path in paths {
-            dbg!(format!("{}", path.unwrap().path().display()));
-        };
-
-        dbg!("one level up");
-
-        let paths = std::fs::read_dir("../").unwrap();
-
-        for path in paths {
-            dbg!(format!("{}", path.unwrap().path().display()));
-        };
-
-        dbg!("two levels up");
-
-        let paths = std::fs::read_dir("../../").unwrap();
-
-        for path in paths {
-            dbg!(format!("{}", path.unwrap().path().display()));
-        };
-
-        dbg!("frontend");
-
-        let paths = std::fs::read_dir("../../frontend/").unwrap();
-
-        for path in paths {
-            dbg!(format!("{}", path.unwrap().path().display()));
-        };
 
     resource_dir(path).build()
 }

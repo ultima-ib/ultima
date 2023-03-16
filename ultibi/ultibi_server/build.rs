b848471a27303326c30f382d4dd607dae79a6b99
Anatoly Bugakov <anatoly.bugakov@outlook.com>
2023-03-16 15:29:43 +0000

    build npm before rust tests and before rust lints #122

--- ultibi/ultibi_server/build.rs
+++ ultibi/ultibi_server/build.rs
@@ -1,8 +1,17 @@
+use std::path::Path;
+
 use static_files::resource_dir;
 
 fn main() -> std::io::Result<()> {
-    let path =
-        std::env::var("STATIC_FILES_DIR").unwrap_or_else(|_| "../../frontend/dist".to_string());
+    let path = std::env::var("STATIC_FILES_DIR").unwrap_or_else(|_| {
+        Path::new("..")
+            .join("..")
+            .join("frontend")
+            .join("dist")
+            .into_os_string()
+            .into_string()
+            .unwrap()
+    });
 
     resource_dir(dbg!(path)).build()
 }

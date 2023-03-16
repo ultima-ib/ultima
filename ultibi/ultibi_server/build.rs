452f2f5f38b74784d432ae94a6e9d2bf95b6c5cf
Anatoly Bugakov <anatoly.bugakov@outlook.com>
2023-03-16 14:22:38 +0000

    dbg

--- ultibi/ultibi_server/build.rs
+++ ultibi/ultibi_server/build.rs
@@ -3,6 +3,19 @@ use static_files::resource_dir;
 fn main() -> std::io::Result<()> {
     let path =
         std::env::var("STATIC_FILES_DIR").unwrap_or_else(|_| "../../frontend/dist".to_string());
-    dbg!(&path);
+        let paths = std::fs::read_dir("./").unwrap();
+
+        for path in paths {
+            dbg!(format!("{}", path.unwrap().path().display()));
+        };
+
+        dbg!("two levels up");
+
+        let paths = std::fs::read_dir("../").unwrap();
+
+        for path in paths {
+            dbg!(format!("{}", path.unwrap().path().display()));
+        };
+
     resource_dir(path).build()
 }

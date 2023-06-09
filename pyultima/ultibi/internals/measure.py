05fe894ea45126ab2a22a681bd4e165fcc52e303
Anatoly Bugakov <anatoly.bugakov@outlook.com>
2023-06-09 22:44:26 +0100

    fmt

--- pyultima/ultibi/internals/measure.py
+++ pyultima/ultibi/internals/measure.py
@@ -7,8 +7,11 @@ from polars import DataType, Expr, Series
 
 from ultibi.internals.filters import Filter
 
-from ..rust_module.ultibi_engine import (CalcParamWrapper, CalculatorWrapper, 
-                                         MeasureWrapper)
+from ..rust_module.ultibi_engine import (
+    CalcParamWrapper,
+    CalculatorWrapper,
+    MeasureWrapper,
+)
 
 
 class CalcParam:

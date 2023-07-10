14de0e56a2039400709ce4ea00a362296c9c741c
Anatoly Bugakov <anatoly.bugakov@outlook.com>
2023-07-10 21:08:45 +0100

    datsource py

--- pyultima/src/datasource.rs
+++ pyultima/src/datasource.rs
@@ -1,5 +1,8 @@
-use pyo3::{pyclass, pymethods};
-use ultibi::{datasource::DataSource};
+use polars::series::Series;
+use pyo3::{pyclass, pymethods, types::PyType, Python, PyAny, Py, PyResult};
+use ultibi::{datasource::DataSource, DataFrame};
+
+use crate::{conversions::series::py_series_to_rust_series, errors::PyUltimaErr};
 
 
 #[pyclass]
@@ -9,4 +12,21 @@ pub struct DataSourceWrapper {
     pub inner: DataSource,
 }
 #[pymethods]
-impl DataSourceWrapper { }
+impl DataSourceWrapper { 
+
+    /// In Memory
+    #[classmethod]
+    fn from_frame(_: &PyType,
+        py: Python,
+        seriess: Vec<Py<PyAny>>) -> PyResult<Self> {
+            let df = DataFrame::new(
+                seriess
+                    .into_iter()
+                    .map(|x| py_series_to_rust_series(x.as_ref(py)))
+                    .collect::<PyResult<Vec<Series>>>()?,
+            )
+            .map_err(PyUltimaErr::Polars)?;
+
+        Ok(DataSourceWrapper{inner: DataSource::InMemory(df)})
+    }
+}

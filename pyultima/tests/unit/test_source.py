6a31945600da1c835eefe2e4404b01a03b30e395
Anatoly Bugakov <anatoly.bugakov@outlook.com>
2023-07-11 20:39:48 +0100

    py datasource test

---
+++ pyultima/tests/unit/test_source.py
@@ -0,0 +1,15 @@
+import unittest
+
+import polars as pl
+
+import ultibi as ul
+
+class TestCreation(unittest.TestCase):
+    # TODO add tast for Scan
+    def test_scan(self) -> None:
+        scan = pl.scan_csv("../frtb_engine/data/frtb/Delta.csv", 
+                           dtypes={"SensitivitySpot": pl.Float64})
+        ul.DataSource(scan)
+
+if __name__ == "__main__":
+    unittest.main()
